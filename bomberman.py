7ec4c12a4e558e4faef5c3fdc4c016f8f6304ced
ravenstudios <ravenstudiosrob@yahoo.com>
2021-01-11 14:17:44 -0600

    removed map from the objects list

--- bomberman.py
+++ bomberman.py
@@ -3,7 +3,8 @@ from main_block import *
 class Bomberman(Main_block):
 
     def __init__(self):
-        self.x, self.y = 1 * BLOCK_SIZE, 1 * BLOCK_SIZE
+        self.x, self.y = 0, 0
+
         super().__init__(self.x, self.y)
     """
     # TODO:
@@ -13,4 +14,22 @@ class Bomberman(Main_block):
         will use unpassable blocks for that.
     """
     def update(self, objects):
-        pass
+        self.key_input()
+        # for obj in objects:
+        #     self.collide(obj.get_rect())
+
+    def key_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_s] or keys[pygame.K_DOWN] :
+            self.rect = self.rect.move(0, self.speed)
+
+
+        if keys[pygame.K_w] or keys[pygame.K_UP] :
+            self.rect = self.rect.move(0, -self.speed)
+
+        if keys[pygame.K_a] or keys[pygame.K_LEFT] :
+            self.rect = self.rect.move(-self.speed, 0)
+
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT] :
+            self.rect = self.rect.move(self.speed, 0)
