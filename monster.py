aedac689606fb7cbccadf5e41b762a929697478c
Jordan <>
2021-01-17 02:13:42 -0600

    made a monster

--- monster.py
+++ monster.py
@@ -1,25 +1,50 @@
+#author Jordan, Dodson, https://github.com/MrBizChicken
 from main_block import *
+import map
 
 class Monster(Main_block):
 
     def __init__(self):
 
 
-        super().__init__(BLOCK_SIZE * 8,  BLOCK_SIZE * 9)
+        super().__init__(BLOCK_SIZE * 7,  BLOCK_SIZE * 9)
 
-        self.speed = -5
+        self.speed = 3
 
 
     def update(self, objects):
-        self.move_vert()
+        self.move_horz(objects)
 
 
-    def move_vert(self):
+    def move_vert(self, objects):
 
+
+        #this function moves vertically and bounces back
+
+
+        for obj in objects:
+            if isinstance(obj, map.Map):
+                map_objects = obj.get_blocks()
+        if not self.walk((0, self.speed), map_objects):
             self.rect = self.rect.move(0, self.speed)
+        else:
+            self.speed = -self.speed
 
-            if self.rect.y <= 0:
-                self.speed = -self.speed
 
-            if self.rect.y >= BLOCK_SIZE * 8 :
+    def move_horz(self, objects):
+
+
+        #this function moves horizontally and bounces back
+
+        for obj in objects:
+            if isinstance(obj, map.Map):
+                map_objects = obj.get_blocks()
+        if not self.walk((self.speed, 0), map_objects):
+            self.rect = self.rect.move(self.speed, 0)
+        else:
+            self.speed = -self.speed
+
+
+            if self.rect.y >= GAME_HEIGHT + BLOCK_SIZE:
+
                 self.speed = -self.speed
