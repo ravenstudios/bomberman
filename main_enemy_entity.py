7574522c1bb0a05c1468b65ba2ddded16356ecaa
ravenstudios <ravenstudiosrob@yahoo.com>
2022-06-06 03:04:49 -0500

    backing up work on ai, still now done

--- main_enemy_entity.py
+++ main_enemy_entity.py
@@ -28,62 +28,57 @@ class Main_enemy_entity(Main_mob_entity):
 
     def ai(self, collideable_objects, crates_group, bombs_group):
 
+        for sprite in collideable_objects:
+            next_step = sprite.rect.colliderect(self.rect.move(0, self.rect.x + self.direction.x * self.speed))
+            if not next_step:
+                self.rect.x += self.direction.x * self.speed
+            else:
+                if self.direction.x > 0:
+                    self.rect.right = next_step.rect.left
+                elif self.direction.x < 0:
+                    self.rect.left = next_step.rect.right
+            next_step = sprite.rect.colliderect(self.rect.move(0, self.rect.y + self.direction.y * self.speed))
+            if not next_step:
+                self.rect.y += self.direction.y * self.speed
+            else:
+                if self.direction.y > 0:
+                    self.rect.bottom = next_step.rect.top
+                elif self.direction.y < 0:
+                    self.rect.top = next_step.rect.bottom
 
-        self.rect.x += self.direction.x * self.speed
-        x_collision = self.check_collision(collideable_objects, "horizontal")
-        if x_collision:
-            if type(x_collision) == crate.Crate:
-                self.direction = pygame.math.Vector2(0, 0)
-                bombs_group.add(bomb.Bomb(self.rect.x, self.rect.y, self.fire_length))
-                escape_locations = self.get_escape_locations(self.rect)
-
-                for sprite in collideable_objects:
-                    for location in escape_locations:
-                        if sprite.rect.x == location[0] and sprite.rect.y == location[1]:
-                            escape_locations.remove(location)
-
-                if escape_locations:
-                    self.rect.x = escape_locations[0][0]
-                    self.rect.y = escape_locations[0][1]
-                    self.direction = self.random_direction()
-                    return
-                else:
-                    self.direction = self.random_direction()
-                    return
-
-            elif type(x_collision) == border_block.Border_block:
-                self.direction.x = -self.direction.x
-                return
-
-
-        self.rect.y += self.direction.y * self.speed
-        y_collision = self.check_collision(collideable_objects, "vertical")
-        if y_collision:
-
-            if type(y_collision) == crate.Crate:
-                self.direction = pygame.math.Vector2(0, 0)
-                bombs_group.add(bomb.Bomb(self.rect.x, self.rect.y, self.fire_length))
-                escape_locations = self.get_escape_locations(self.rect)
-
-                for sprite in collideable_objects:
-                    for location in escape_locations:
-                        if sprite.rect.x == location[0] and sprite.rect.y == location[1]:
-                            escape_locations.remove(location)
-
-                if escape_locations:
-                    self.rect.x = escape_locations[0][0]
-                    self.rect.y = escape_locations[0][1]
-                    self.direction = self.random_direction()
-                    return
-                else:
-                    self.direction = self.random_direction()
-                    return
-
-            elif type(y_collision) == border_block.Border_block:
-                self.direction.y = -self.direction.y
-                return
-
-
+        # x_collision = self.check_collision(collideable_objects, "horizontal")
+        #
+        # self.rect.y += self.direction.y * self.speed
+        # y_collision = self.check_collision(collideable_objects, "vertical")
+        #
+        # if x_collision or y_collision:
+        #     if type(x_collision) == crate.Crate or type(y_collision) == crate.Crate:
+        #         self.direction = pygame.math.Vector2(0, 0)
+        #         bombs_group.add(bomb.Bomb(self.rect.x, self.rect.y, self.fire_length))
+        #         escape_locations = self.get_escape_locations(self.rect)
+        #
+        #         for sprite in collideable_objects:
+        #             for location in escape_locations:
+        #                 if sprite.rect.x == location[0] and sprite.rect.y == location[1]:
+        #                     escape_locations.remove(location)
+        #
+        #         if escape_locations:
+        #             # self.rect.x = escape_locations[0][0]
+        #             # self.rect.y = escape_locations[0][1]
+        #             self.direction = self.random_direction()
+        #             return
+        #         else:
+        #             self.direction = self.random_direction()
+        #             return
+        #
+        #     elif type(x_collision) == border_block.Border_block or type(y_collision) == border_block.Border_block:
+        #         self.direction = self.random_direction()
+        #         return
+        #     # elif type(y_collision) == border_block.Border_block:
+        #     #     self.direction.y = -self.direction.y
+        #     #     return
+        #
+        #
 
 
 
